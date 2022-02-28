e0f39933c2216180a19eed78ed66c666506af836
JorgeVargas <javargas216@unicauca.edu.co>
2022-02-27 22:48:24 -0500

    Slide in right menu animation

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 import { Spin as Hamburger } from 'hamburger-react'
+import 'animate.css';
 import './navbar.css'
+
 
 
 export default function Navbar() {
 
     const [isOpen, setOpen] = useState(false)
+    const [anim, setAnim] = useState('animate__animated animate__slideInRight')
 
     return (
         <div className='navbar'>
@@ -13,9 +16,9 @@ export default function Navbar() {
                 <h3>JV</h3>
 
                 <ul
-                    className={isOpen ? 'navlinks-mobile' : 'navlinks'}
+                    className={isOpen ? `navlinks-mobile ${anim}` : 'navlinks'}
                     onClick={() => setOpen(false)}
-                >
+                >               
 
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
