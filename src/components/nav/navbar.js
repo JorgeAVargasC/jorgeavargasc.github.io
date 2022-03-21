204885dcf2ab2e7db6734db9967f15d92e01f809
JorgeVargas <javargas216@unicauca.edu.co>
2022-03-20 23:09:36 -0500

    SVG logo navbar

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Spin as Hamburger } from 'hamburger-react'
 import './navbar.css'
-
+import Logo2 from '../../assets/svg/Logo2.svg'
 
 
 export default function Navbar() {
@@ -11,12 +11,15 @@ export default function Navbar() {
     return (
         <div className='navbar'>
             <nav>
-                <h3>JV</h3>
+                <a href='#home'>
+                    <img className="logo" src={Logo2} alt="" />
+                </a>
+
 
                 <ul
                     className={isOpen ? 'navlinks-mobile' : 'navlinks'}
                     onClick={() => setOpen(false)}
-                >               
+                >
 
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
