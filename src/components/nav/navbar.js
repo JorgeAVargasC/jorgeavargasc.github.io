d80a51b138376c4968d607f80939b804bf89f09c
JorgeVargas <javargas216@unicauca.edu.co>
2022-05-09 16:15:57 -0500

    Open and close mobile menu animation with useState

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { Spin as Hamburger } from 'hamburger-react'
-import './navbar.css'
+
 import Logo2 from '../../assets/svg/Logo2.svg'
+import './navbar.css'
 
 export default function Navbar() {
 
@@ -9,22 +10,32 @@ export default function Navbar() {
 
     return (
         <div className='navbar'>
+            
             <div className='navbar-content'>
                 <a href='#home'>
                     <img className="logo" src={Logo2} alt="" />
                 </a>
-                <nav className={isOpen ? 'navlinks-mobile' : 'navlinks'} onClick={() => setOpen(false)}>
+
+                <nav className={isOpen ? 'navlinks-mobile open' : 'navlinks-mobile close'} onClick={() => setOpen(false)}>
                     <a className='links' href="#home">Home</a>
                     <a className='links' href="#about">About</a>
                     <a className='links' href="#contact">Contact</a>
                 </nav>
+
                 <Hamburger
                     rounded
                     toggled={isOpen}
                     toggle={setOpen}
                     direction='left'
-                    duration={0.6}
+                    duration={0.4}
                 />
+
+                <nav className='navlinks'>
+                    <a className='links' href="#home">Home</a>
+                    <a className='links' href="#about">About</a>
+                    <a className='links' href="#contact">Contact</a>
+                </nav>
+                
             </div>
         </div>
     )
