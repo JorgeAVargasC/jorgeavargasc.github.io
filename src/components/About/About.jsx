427a2e5bb0b8fcf062081eedba25494a0ba24e72
JorgeVargas <javargas216@unicauca.edu.co>
2022-08-18 17:31:17 -0500

    about section v1

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,19 +1,50 @@
 import React from "react";
-// import styles from "./About.module.css";
+import styles from "./About.module.css";
+import getAge from "../../functions/getAge";
 import { MdPersonSearch } from "react-icons/md";
 
 import mydata from "../../data/mydata";
 
 export default function About() {
+
+	const birthday = new Date(mydata.birthday).toLocaleDateString('en-us', { day:"numeric", year:"numeric", month:"short"});
+
 	return (
 		<div id="about" className="container">
 			
 			<h3 className="mb-2">{mydata.name}</h3>
-			<h4 className="mb-2">A <span className="txt-blue">fullstack developer</span> from <span className="txt-blue">Colombia</span></h4>
 
-			<p>{mydata.bio}</p>
+			<h4 className="mb-2">I'm a <span className="txt-blue">{mydata.role}</span> from <span className="txt-blue">{mydata.country}</span></h4>
 
-			
+			<p className="mb-2">{mydata.shortbio}</p>
+
+			<div className={styles.about_list}>
+
+				<div className={styles.item}>
+					<p className="">Birthday</p>
+					<p>{birthday}</p>
+				</div>
+				<div className={styles.item}>
+					<p className="">Age</p>
+					<p>{getAge(mydata.birthday)}</p>
+				</div>
+				<div className={styles.item}>
+					<p className="">Phone</p>
+					<p>{mydata.phone}</p>
+				</div>
+				<div className={styles.item}>
+					<p className="">E-mail</p>
+					<p>{mydata.email}</p>
+				</div>
+				<div className={styles.item}>
+					<p className="">Phone</p>
+					<p>{mydata.phone}</p>
+				</div>
+				<div className={styles.item}>
+					<p className="">Phone</p>
+					<p>{mydata.phone}</p>
+				</div>
+			</div>
 
 			{/* <h1 className="mb-2">About</h1> */}
 			{/* <div className="card">
---
+++ src/functions/getAge.js
@@ -0,0 +1,12 @@
+export default function getAge(date){
+    let today = new Date();
+    let birthday = new Date(date);
+    let age = today.getFullYear() - birthday.getFullYear();
+    let m = today.getMonth() - birthday.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < birthday.getDate())) {
+        age--;
+    }
+
+    return age;
+}
