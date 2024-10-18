4b17f1da78941e2410f2154b03d4e99a55285a6a
JorgeVargasConnectUp <jorge.vargas@connectupweb.com>
2024-10-17 23:24:51 -0500

    refactor: gh pages

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { NextUIProvider } from '@nextui-org/react'
 import { HomePage } from './pages'
 
-export default function MeApp() {
+export default function App() {
 	return (
 		<NextUIProvider>
 			<main className='text-foreground bg-background dark min-h-screen'>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import MeApp from './me-app.tsx'
+import App from './app.tsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<MeApp />
+		<App />
 	</StrictMode>
 )
